e6e25d85ca60d42c6a8c543df23a95002c5ec3a2
Alokkumar1411 <2k24.cs1c.2414120@gmail.com>
2026-06-08 18:08:48 +0530

    Added new changes

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -3,24 +3,16 @@ import React from 'react'
 const TaskList = () => {
   return (
     <div id='tasklist' className='mt-10 h-[55%] flex overflow-x-auto items-center justify-start gap-5 flex-nowrap py-5 w-full'>
-      <div className='shrink-0 h-full w-75 rounded-xl bg-yellow-200'>
+      <div className='shrink-0 h-full w-75 p-5 rounded-xl bg-gray-400'>
+        <div className='flex justify-between text-center' >
+          <h3 className='bg-red-600 text-sm px-3 py-1 rounded-lg'>High</h3>
+          <h4 className='text-sm'>08 June 2026</h4>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold'>Make a Good Portfolio</h2>
+        <p className='mt-3 text-base' > Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut placeat in laudantium optio quod est!</p>
 
       </div>
-      <div className='shrink-0 h-full w-75 rounded-xl bg-yellow-200'>
-
-      </div>
-      <div className='shrink-0 h-full w-75 rounded-xl bg-yellow-200'>
-
-      </div>
-      <div className='shrink-0 h-full w-75 rounded-xl bg-yellow-200'>
-
-      </div>
-      <div className='shrink-0 h-full w-75 rounded-xl bg-yellow-200'>
-
-      </div>
-      <div className='shrink-0 h-full w-75 rounded-xl bg-yellow-200'>
-
-      </div>
+      
     </div>
   )
 }
---
+++ src/components/others/AllTask.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AllTask = () => {
+  return (
+    <div id='task' className='bg-[#1c1c1c] p-4 rounded mt-5 overflow-auto h-48 ' >
+      <div className='bg-red-400 py-2 px-4 flex rounded mb-3 justify-between ' >
+        <h2>ALok</h2>
+        <h3>Make a UI Design</h3>
+        <h5>Status</h5>
+      </div>
+    </div>
+  )
+}
+
+export default AllTask
